2fd8028b2822eaa17de429c0f80e5ccdc35e2742
Mihail Tratsiuk <mihail.tratsiuk@gmail.com>
2019-06-08 21:45:10 +0300

    Add classes for ast nodes

---
+++ src/expr.ts
@@ -0,0 +1,46 @@
+// Generated code
+
+import { Token, Literal as LiteralValue } from "./token"
+
+export interface Visitor<T> {
+  visitBinaryExpr(expr: Binary): T
+  visitGroupingExpr(expr: Grouping): T
+  visitLiteralExpr(expr: Literal): T
+  visitUnaryExpr(expr: Unary): T
+}
+
+export interface Expr {
+  accept<T>(visitor: Visitor<T>): T
+}
+
+export class Binary implements Expr {
+  constructor(readonly left: Expr, readonly operator: Token, readonly right: Expr) {}
+
+  accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitBinaryExpr(this)
+  }
+}
+
+export class Grouping implements Expr {
+  constructor(readonly expression: Expr) {}
+
+  accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitGroupingExpr(this)
+  }
+}
+
+export class Literal implements Expr {
+  constructor(readonly value: LiteralValue) {}
+
+  accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitLiteralExpr(this)
+  }
+}
+
+export class Unary implements Expr {
+  constructor(readonly operator: Token, readonly right: Expr) {}
+
+  accept<T>(visitor: Visitor<T>): T {
+    return visitor.visitUnaryExpr(this)
+  }
+}
---
+++ src/generate-ast.ts
@@ -0,0 +1,63 @@
+import * as fs from 'fs'
+
+main(process.argv)
+
+function main(args: Array<string>): void {
+  fs.writeFile(args[2], defineAst(), error => {
+    if (error) {
+      console.log(error)
+      process.exit(1)
+    }
+  })
+}
+
+function defineAst (): string {
+  enum Types {
+    Expr = 'Expr',
+    Token = 'Token',
+    Literal = 'LiteralValue',
+  }
+
+  const nodesDef: Array<[string, ...Array<[Types, string]>]> = [
+    ['Binary', [Types.Expr, 'left'], [Types.Token, 'operator'], [Types.Expr, 'right']],
+    ['Grouping', [Types.Expr, 'expression']],
+    ['Literal', [Types.Literal, 'value']],
+    ['Unary', [Types.Token, 'operator'], [Types.Expr, 'right']]
+  ]
+
+  const expr = `\
+export interface ${Types.Expr} {
+  accept<T>(visitor: Visitor<T>): T
+}
+`
+
+  const visitor = `\
+export interface Visitor<T> {
+  ${nodesDef.map(([name]) => `visit${name}${Types.Expr}(expr: ${name}): T`).join('\n')}
+}
+`
+
+  const nodes = nodesDef.map(([name, ...args]) => `\
+export class ${name} implements ${Types.Expr} {
+  constructor(
+    ${args.map(([type, name]) => `readonly ${name}: ${type}`).join(', \n')}
+  ) {}
+
+  accept<T>(visitor: Visitor<T>): T {
+    return visitor.visit${name}${Types.Expr}(this)
+  }
+}
+`).join('\n')
+
+  return `\
+// Generated code
+
+import { Token, Literal as LiteralValue } from "./token"
+
+${visitor}
+
+${expr}
+
+${nodes}
+`
+}
